f665da37e521901ae6f10671100429ce131de96b
Lucas Nascimento <2006lucasns@gmail.com>
2025-12-17 11:14:26 -0300

    fix: anotacoes no DTO de resposta

--- src/main/java/equipe25/churninsight_backend/dto/DataPredictResposta.java
+++ src/main/java/equipe25/churninsight_backend/dto/DataPredictResposta.java
@@ -6,15 +6,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DataPredictResposta(
-        //Enum
-        @NotNull
         PrevisaoChurn previsaoChurn,
-        @NotNull
         Float probabilidade,
-        //Enum
-        @NotNull
         NivelRisco nivelRisco,
-        @NotBlank
         String recomendacao
 ){
 
