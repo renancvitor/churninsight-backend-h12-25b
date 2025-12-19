000d5cca3ddcfda2a1d89e9d27d45cb530f91b68
Gabriel <oliveirafrerreira97@hotmail.com>
2025-12-18 23:00:43 -0300

    fix(dto): refatora DTO de entrada e corrige validações de cliente
    orreção da  estrutura do DTO EntradaCliente para o tipo de entrada esperada pelo time DS

--- src/main/java/equipe25/churninsight_backend/dto/EntradaCliente.java
+++ src/main/java/equipe25/churninsight_backend/dto/EntradaCliente.java
@@ -1,25 +1,21 @@
 package equipe25.churninsight_backend.dto;
 
-import equipe25.churninsight_backend.enuns.TipoPlano;
 import jakarta.validation.constraints.*;
 
 public record EntradaCliente(
         @NotNull
-        @Min(value = 1, message = "O tempo de contrato deve ser maior que 1")
-        @Max(240)
-        Integer tempoContratoMeses,
-
+        String CreditScore,
         @NotNull
-        @PositiveOrZero
-        @Max(value = 3,message = "O número máximo de atrasos permitido é 3, acima disso, encerre ou bloqueie assinatura")
-        Integer atrasosPagamento,
-
+        String Geography,
         @NotNull
-        @DecimalMin(value = "0.0", inclusive = true)
-        @DecimalMax(value = "1000.0", inclusive = true)
-        Double usoMensal,
-
+        String Gender,
         @NotNull
-        TipoPlano plano
+        String Age ,
+        @NotNull
+        String  Tenure,
+        @NotNull
+        String Balance,
+        @NotNull
+        String  EstimatedSalary
 ) {
 }
