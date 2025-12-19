dde0f70763080cfad0571b6ccebfddd176a44cad
renancvitor <renan.vitor.cm@gmail.com>
2025-12-19 19:09:37 -0300

    refactor: adiciona instrução de correção da entidade

--- src/main/java/equipe25/churninsight_backend/model/PredicaoChurnEntity.java
+++ src/main/java/equipe25/churninsight_backend/model/PredicaoChurnEntity.java
@@ -1,6 +1,7 @@
 package equipe25.churninsight_backend.model;
 
 public class PredicaoChurnEntity {
+<<<<<<< Updated upstream
 
     /*
      * Incluir as anotações acima no nome da classe, como entity, getter, setter,
@@ -11,4 +12,28 @@ public class PredicaoChurnEntity {
      * Incluir todas as annotation necessárias nesta entidade
      */
 
+=======
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    // private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PrevisaoChurnEnum previsao;
+    // private PrevisaoChrunEntidade previsao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NivelRiscoEnum nivelRisco;
+    // private NivelRiscoEntidade nivelRisco;
+
+    @Column(nullable = false)
+    private Double probabilidade;
+
+    @Column(nullable = false)
+    private String recomendacao;
+
+>>>>>>> Stashed changes
 }
