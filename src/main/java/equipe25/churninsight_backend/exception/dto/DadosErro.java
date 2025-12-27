cde805f70a901328fe82002ab1af9c2c934cc5de
Gabriel <oliveirafrerreira97@hotmail.com>
2025-12-26 23:42:18 -0300

    feat(exception): implementação da  Global Exception Handler centralizado
    - Adicionado o  @RestControllerAdvice para capturar erros
    - Criados DTOs (record) para padronização de respostas JSON
    - Implementado tratamentos para 404 (NotFound), 400 (BadRequest) e 500 (Internal)

---
+++ src/main/java/equipe25/churninsight_backend/exception/dto/DadosErro.java
@@ -0,0 +1,4 @@
+package equipe25.churninsight_backend.exception.dto;
+
+public record DadosErro(String mensagem, Integer status) {
+}
---
+++ src/main/java/equipe25/churninsight_backend/exception/dto/DadosErroValidacao.java
@@ -0,0 +1,4 @@
+package equipe25.churninsight_backend.exception.dto;
+
+public record DadosErroValidacao(String campo, String mensagem) {
+}
