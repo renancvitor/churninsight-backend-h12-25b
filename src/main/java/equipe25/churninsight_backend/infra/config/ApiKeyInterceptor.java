3bc940b782c4872c7396c6c74d0b7ddbca2b3e50
renancvitor <renan.vitor.cm@gmail.com>
2025-12-30 17:55:26 -0300

    refactor: nega profile de test para ApiKeyInterceptor e atribui profile de test para ChurninsightBackendApplicationTests

--- src/main/java/equipe25/churninsight_backend/infra/config/ApiKeyInterceptor.java
+++ src/main/java/equipe25/churninsight_backend/infra/config/ApiKeyInterceptor.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -14,6 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
+@Profile("!test")
 public class ApiKeyInterceptor implements HandlerInterceptor {
 
     private final Set<String> validKeys;
--- src/test/java/equipe25/churninsight_backend/ChurninsightBackendApplicationTests.java
+++ src/test/java/equipe25/churninsight_backend/ChurninsightBackendApplicationTests.java
@@ -2,8 +2,10 @@ package equipe25.churninsight_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChurninsightBackendApplicationTests {
 
 	@Test
