e2a1b0daa685bd5f48f26269240c5dbc10e2c0e1
Lucas Nascimento <2006lucasns@gmail.com>
2025-12-30 13:07:48 -0300

    feat: total de previsao e fix: obter grafico

--- src/main/java/equipe25/churninsight_backend/application/previsao/dto/PrevisaoPorNivelRisco.java
+++ src/main/java/equipe25/churninsight_backend/application/previsao/dto/PrevisaoPorNivelRisco.java
@@ -3,6 +3,6 @@ package equipe25.churninsight_backend.application.previsao.dto;
 import equipe25.churninsight_backend.model.nivelrisco.NivelRiscoEntidade;
 
 public record PrevisaoPorNivelRisco(
-                NivelRiscoEntidade nivelRisco,
+                String nivelRisco,
                 Long quantidade) {
 }
