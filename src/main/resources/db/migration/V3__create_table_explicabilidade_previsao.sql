27ff976ca5625e3b87963ef326dd26e47b656104
renancvitor <renan.vitor.cm@gmail.com>
2026-01-03 14:24:45 -0300

    feat: adiciona migration para criar tabela explicabilidade_previsao

---
+++ src/main/resources/db/migration/V3__create_table_explicabilidade_previsao.sql
@@ -0,0 +1,6 @@
+CREATE TABLE explicacao_previsao (
+  previsao_id BIGINT NOT NULL,
+  fator VARCHAR(100) NOT NULL,
+  PRIMARY KEY (previsao_id, fator),
+  FOREIGN KEY (previsao_id) REFERENCES previsao(id)
+);
