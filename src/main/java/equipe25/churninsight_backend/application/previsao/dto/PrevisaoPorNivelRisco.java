6beee963a7711331e30d455fc5476904c2a2913d
renancvitor <renan.vitor.cm@gmail.com>
2026-01-03 14:38:50 -0300

    refactor: remove import sem uso

--- src/main/java/equipe25/churninsight_backend/application/previsao/dto/PrevisaoPorNivelRisco.java
+++ src/main/java/equipe25/churninsight_backend/application/previsao/dto/PrevisaoPorNivelRisco.java
@@ -1,8 +1,6 @@
 package equipe25.churninsight_backend.application.previsao.dto;
 
-import equipe25.churninsight_backend.model.nivelrisco.NivelRiscoEntidade;
-
 public record PrevisaoPorNivelRisco(
-                String nivelRisco,
-                Long quantidade) {
+        String nivelRisco,
+        Long quantidade) {
 }
