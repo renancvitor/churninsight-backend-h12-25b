52228733dfce3f9a95609cb72a72ceac75c11769
renancvitor <renan.vitor.cm@gmail.com>
2026-01-03 16:14:39 -0300

    feat: adiciona enum para traduzir valores

---
+++ src/main/java/equipe25/churninsight_backend/model/previsao/ExplicabilidadeEnum.java
@@ -0,0 +1,44 @@
+package equipe25.churninsight_backend.model.previsao;
+
+public enum ExplicabilidadeEnum {
+
+    AGE("Age", "Idade"),
+    BALANCE("Balance", "Saldo"),
+    CREDIT_SCORE("CreditScore", "Score de Crédito"),
+    TENURE("Tenure", "Tempo de Relacionamento"),
+    ESTIMATED_SALARY("EstimatedSalary", "Salário Estimado"),
+
+    SPAIN("SPAIN", "Espanha"),
+    FRANCE("FRANCE", "França"),
+    GERMANY("GERMANY", "Alemanha"),
+
+    MALE("MALE", "Masculino"),
+    FEMALE("FEMALE", "Feminino");
+
+    private final String valorBancoDeDados;
+    private final String displayName;
+
+    ExplicabilidadeEnum(String valorBancoDeDados, String displayName) {
+        this.valorBancoDeDados = valorBancoDeDados;
+        this.displayName = displayName;
+    }
+
+    public String getValorBancoDeDados() {
+        return valorBancoDeDados;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public static String traduzir(String valor) {
+        for (ExplicabilidadeEnum explicabilidade : values()) {
+            if (explicabilidade.valorBancoDeDados.equals(valor)) {
+                return explicabilidade.displayName;
+            }
+        }
+
+        return valor;
+    }
+
+}
