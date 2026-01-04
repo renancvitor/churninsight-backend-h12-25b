6761aee24ecb7f462ae9ef0b12781cf87761058b
renancvitor <renan.vitor.cm@gmail.com>
2026-01-03 22:26:19 -0300

    refactor: adiciona validações conforme contrato Data Science

--- src/main/java/equipe25/churninsight_backend/application/api/dto/ClienteRequest.java
+++ src/main/java/equipe25/churninsight_backend/application/api/dto/ClienteRequest.java
@@ -7,11 +7,11 @@ import equipe25.churninsight_backend.model.pais.enums.PaisEnum;
 import jakarta.validation.constraints.*;
 
 public record ClienteRequest(
-                @NotNull @JsonProperty("CreditScore") Integer creditScore,
-                @NotNull @JsonProperty("Geography") PaisEnum geography,
-                @NotNull @JsonProperty("Gender") GeneroEnum gender,
-                @NotNull @Min(value = 18, message = "Idade não pode ser menor que 18") @Max(value = 120, message = "Idade inválida") @JsonProperty("Age") Integer age,
-                @NotNull @JsonProperty("Tenure") Integer tenure,
-                @NotNull @JsonProperty("Balance") Double balance,
-                @NotNull @JsonProperty("EstimatedSalary") Double estimatedSalary) {
+        @NotNull @Min(value = 350, message = "CreditScore deve ser no mínimo 350") @Max(value = 850, message = "CreditScore deve ser no máximo 850") @JsonProperty("CreditScore") Integer creditScore,
+        @NotNull @JsonProperty("Geography") PaisEnum geography,
+        @NotNull @JsonProperty("Gender") GeneroEnum gender,
+        @NotNull @Min(value = 18, message = "Idade não pode ser menor que 18") @Max(value = 92, message = "Idade não pode ser maior que 92") @JsonProperty("Age") Integer age,
+        @NotNull @Min(value = 0, message = "Tenure não pode ser menor que 0") @Max(value = 10, message = "Tenure não pode ser maior que 10") @JsonProperty("Tenure") Integer tenure,
+        @NotNull @DecimalMin(value = "0.00", inclusive = true, message = "Balance não pode ser negativo") @DecimalMax(value = "250898.00", inclusive = true, message = "Balance excede o valor máximo permitido") @JsonProperty("Balance") Double balance,
+        @NotNull @DecimalMin(value = "11.58", inclusive = true, message = "EstimatedSalary abaixo do mínimo permitido") @DecimalMax(value = "199992.48", inclusive = true, message = "EstimatedSalary acima do máximo permitido") @JsonProperty("EstimatedSalary") Double estimatedSalary) {
 }
