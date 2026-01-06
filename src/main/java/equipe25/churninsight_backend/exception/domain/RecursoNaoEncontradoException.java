c511c65061b00f5d0c5abdc2d6ce982e2cff47f4
Gabriel <oliveirafrerreira97@hotmail.com>
2026-01-06 20:27:32 -0300

    feat: atualizado o tratador de erros e adicionado exceções de dominio
    issue 29

---
+++ src/main/java/equipe25/churninsight_backend/exception/domain/RecursoNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package equipe25.churninsight_backend.exception.domain;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
---
+++ src/main/java/equipe25/churninsight_backend/exception/domain/RegraNegocioException.java
@@ -0,0 +1,9 @@
+package equipe25.churninsight_backend.exception.domain;
+
+public class RegraNegocioException extends RuntimeException{
+
+    public RegraNegocioException(String mensagem) {
+        super(mensagem);
+    }
+
+}
