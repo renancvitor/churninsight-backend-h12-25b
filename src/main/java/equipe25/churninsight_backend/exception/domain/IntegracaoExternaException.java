b8cd9572ef44e310eb3dc918b21fc5c3b5c95411
Gabriel <oliveirafrerreira97@hotmail.com>
2026-01-06 20:19:09 -0300

    feat: adicionar exceções de domínio e atualizar tratador de erros
    Implementa RecursoNaoEncontradoException, RegraNegocioException e IntegracaoExternaException conforme a ussue 29

---
+++ src/main/java/equipe25/churninsight_backend/exception/domain/IntegracaoExternaException.java
@@ -0,0 +1,9 @@
+package equipe25.churninsight_backend.exception.domain;
+
+public class IntegracaoExternaException extends RuntimeException{
+
+    public IntegracaoExternaException(String mensagem) {
+        super(mensagem);
+    }
+
+}
