a83f584ead29be60cdde488f225efea5f41e27c2
renancvitor <renan.vitor.cm@gmail.com>
2026-01-07 21:50:23 -0300

    feat: adiciona novos endpoints para consumo de batch prediction

--- src/main/java/equipe25/churninsight_backend/application/api/service/PrevisaoClienteService.java
+++ src/main/java/equipe25/churninsight_backend/application/api/service/PrevisaoClienteService.java
@@ -1,8 +1,17 @@
 package equipe25.churninsight_backend.application.api.service;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import equipe25.churninsight_backend.application.api.dto.BatchJobResponse;
+import equipe25.churninsight_backend.application.api.dto.BatchStatusResponse;
 import equipe25.churninsight_backend.application.api.dto.ClienteRequest;
 import equipe25.churninsight_backend.application.api.dto.ClienteResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +22,40 @@ public class PrevisaoClienteService {
 
     private final RestTemplate restTemplate;
 
-    private static final String PYTHON_API_URL = "https://churn-hackathon.onrender.com/previsao";
+    private static final String PYTHON_API_URL = "https://churn-hackathon.onrender.com";
 
     public ClienteResponse prever(ClienteRequest request) {
         return restTemplate.postForObject(
-                PYTHON_API_URL,
+                PYTHON_API_URL + "/previsao",
                 request,
                 ClienteResponse.class);
     }
+
+    public BatchJobResponse enviarBatch(MultipartFile file) {
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", file.getResource());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(body, headers);
+
+        return restTemplate.postForObject(
+                PYTHON_API_URL + "/previsao-lote",
+                request,
+                BatchJobResponse.class);
+    }
+
+    public BatchStatusResponse consultarStatus(String jobId) {
+        return restTemplate.getForObject(
+                PYTHON_API_URL + "/previsao-lote/status/" + jobId,
+                BatchStatusResponse.class);
+    }
+
+    public Resource baixarResultado(String jobId) {
+        return restTemplate.getForObject(
+                PYTHON_API_URL + "/previsao-lote/download/" + jobId,
+                Resource.class);
+    }
+
 }
