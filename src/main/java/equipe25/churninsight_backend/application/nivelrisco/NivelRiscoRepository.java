2769d9870bdec958aff60f5e645241d8f384696e
renancvitor <renan.vitor.cm@gmail.com>
2026-01-07 21:49:24 -0300

    feat: adiciona busca por nome

--- src/main/java/equipe25/churninsight_backend/application/nivelrisco/NivelRiscoRepository.java
+++ src/main/java/equipe25/churninsight_backend/application/nivelrisco/NivelRiscoRepository.java
@@ -1,9 +1,13 @@
 package equipe25.churninsight_backend.application.nivelrisco;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import equipe25.churninsight_backend.model.nivelrisco.NivelRiscoEntidade;
 
 public interface NivelRiscoRepository extends JpaRepository<NivelRiscoEntidade, Integer> {
+
+    Optional<NivelRiscoEntidade> findByNivelRiscoNome(String nivelRisco);
 
 }
--- src/main/java/equipe25/churninsight_backend/application/tipoprevisao/TipoPrevisaoRepository.java
+++ src/main/java/equipe25/churninsight_backend/application/tipoprevisao/TipoPrevisaoRepository.java
@@ -1,9 +1,13 @@
 package equipe25.churninsight_backend.application.tipoprevisao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import equipe25.churninsight_backend.model.tipoprevisao.TipoPrevisaoEntidade;
 
 public interface TipoPrevisaoRepository extends JpaRepository<TipoPrevisaoEntidade, Integer> {
+
+    Optional<TipoPrevisaoEntidade> findByTipoPrevisao(String tipoPrevisao);
 
 }
