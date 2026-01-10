82aece8a30d8e23a4f1be26baa969022b929ae17
renancvitor <renan.vitor.cm@gmail.com>
2026-01-10 19:01:08 -0300

    chore: corrige configuração CI/CD para deploy automático na VM

--- src/main/java/equipe25/churninsight_backend/application/previsao/controller/RootController.java
+++ src/main/java/equipe25/churninsight_backend/application/previsao/controller/RootController.java
@@ -13,7 +13,7 @@ public class RootController {
         return Map.of(
                 "status", "online",
                 "service", "ChurnInsight API",
-                "Version", "1.0.4",
+                "Version", "1.0.5",
                 "docs", "/swagger-ui.html",
                 "health", "/health");
     }
