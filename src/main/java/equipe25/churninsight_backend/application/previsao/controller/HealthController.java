4342be0977b271d47e08b43b1f1246815a119c69
renancvitor <renan.vitor.cm@gmail.com>
2026-01-13 22:03:01 -0300

    refactor: integra documentação completa Swagger, endpoint raíz e /heath

--- src/main/java/equipe25/churninsight_backend/application/previsao/controller/HealthController.java
+++ src/main/java/equipe25/churninsight_backend/application/previsao/controller/HealthController.java
@@ -1,5 +1,8 @@
 package equipe25.churninsight_backend.application.previsao.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping
-    public ResponseEntity<String> health() {
-        return ResponseEntity.ok("OK");
+    public ResponseEntity<Map<String, Object>> health() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("application", "ChurnInsight API");
+        response.put("version", "1.0.0");
+
+        return ResponseEntity.ok(response);
     }
+
 }
